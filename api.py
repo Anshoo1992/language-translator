c20b865d73c36d8e163a538388e9c9e5a54d0307
anshpandey <anshoo.pandey@non.schneider-electric.com>
2021-06-01 17:17:19 +0530

    file changed

--- api.py
+++ api.py
@@ -1,4 +1,3 @@
-import os
 import flask
 from flask import request
 import languageTranslator
@@ -24,5 +23,5 @@ def get_translated_text():
 
     translated_text = languageTranslator.translate(input_text, source_lang, target_lang)
     return translated_text, 200
-PORT = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=PORT)
+
+app.run()
